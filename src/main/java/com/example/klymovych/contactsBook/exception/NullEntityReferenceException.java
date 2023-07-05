e431186124bf22614e041f60cc3774db7862a6ce
AlexanderKlimovich <alexanderklimovich1@gmail.com>
2023-07-05 15:32:53 +0300

    write NullEntityReferenceException.java and GlobalExceptionHandler.java

---
+++ src/main/java/com/example/klymovych/contactsBook/exception/NullEntityReferenceException.java
@@ -0,0 +1,9 @@
+package com.example.klymovych.contactsBook.exception;
+
+public class NullEntityReferenceException extends RuntimeException {
+    public NullEntityReferenceException() {    }
+
+    public NullEntityReferenceException(String message) {
+        super(message);
+    }
+}
