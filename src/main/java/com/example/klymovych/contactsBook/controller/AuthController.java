a0080b1f5c5a266f27c9ee6b33ad14b5eff20c89
AlexanderKlimovich <alexanderklimovich1@gmail.com>
2023-07-06 20:17:52 +0300

    add authentication controller

---
+++ src/main/java/com/example/klymovych/contactsBook/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.example.klymovych.contactsBook.controller;
+
+import com.example.klymovych.contactsBook.dto.JwtRequest;
+import com.example.klymovych.contactsBook.dto.RegistrationUserDto;
+import com.example.klymovych.contactsBook.service.Impl.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/auth")
+    public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest authRequest) {
+        return authService.createAuthToken(authRequest);
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<?> createNewUser(@RequestBody RegistrationUserDto registrationUserDto) {
+        return authService.createNewUser(registrationUserDto);
+    }
+}
